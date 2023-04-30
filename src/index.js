a8303de563c3453ab5edeb904d3ac4aa34e9a4a6
toomuchyasar <yasarmuslu93@outlook.com>
2023-04-30 11:57:39 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 import './less/index.less'
 
-// Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
+const headerImg = document.querySelector(".intro img")
 
+headerImg.addEventListener("mouseover", (a) => {
+    a.target.style.filter= "grayscale(1) blur(3px)"}
+)
+headerImg.addEventListener("mouseleave", (a) => {
+    a.target.style.filter= "grayscale(0) blur(0)"}
+);
 
-// Kodlar buraya gelecek!
+const buttons = document.querySelectorAll(".btn")
+
+for(let i of buttons) {
+    i.addEventListener("click", (e) => {
+        alert("Rezervasyon için telefon numaramız : 28923198398721")
+    })
+};
+
+const navs = document.querySelectorAll("nav a")
+
+console.log(navs)
+
+for(let i of navs) {
+    i.addEventListener("mouseover",function(c){
+        i.style.fontWeight = "bold"}
+    )
+    i.addEventListener("mouseleave",function(c){
+       i.style.fontWeight = "normal"}
+    )
+};
